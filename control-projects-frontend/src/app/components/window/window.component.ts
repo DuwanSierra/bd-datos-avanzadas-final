c26526d87ce7852ccb238142521605f3670d7de1
Duwan Sierra <desierrag@gmail.com>
2022-07-20 16:59:36 -0500

    feature: windows events

--- control-projects-frontend/src/app/components/window/window.component.ts
+++ control-projects-frontend/src/app/components/window/window.component.ts
@@ -1,4 +1,14 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { WindowConstants } from './window.constants';
 
 @Component({
   selector: 'window-template',
@@ -6,11 +16,38 @@ import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/cor
   styleUrls: ['./window.component.scss'],
 })
 export class WindowComponent implements OnInit {
-
+  
+  // Inputs
   @Input() title?: string;
   @Input() showActions = false;
+  
+  //Outputs
+  @Output() closeEvent = new EventEmitter<boolean>();
+  @Output() maximiseEvent = new EventEmitter<boolean>();
+  @Output() minimiseEvent = new EventEmitter<boolean>();
+
+  //variables
+  public isFullScreen = true;
+
+  public CONSTANTS = WindowConstants;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  buttonAction(action: string) {
+    switch (action) {
+      case this.CONSTANTS.ACTION_CLOSE:
+        this.closeEvent.emit();
+        break;
+      case this.CONSTANTS.ACTION_MAXIMISE:
+        this.isFullScreen = true;
+        this.maximiseEvent.emit();
+        break;
+      case this.CONSTANTS.ACTION_MINIMISE:
+        this.isFullScreen = false;
+        this.minimiseEvent.emit();
+        break;
+    }
+  }
 }
---
+++ control-projects-frontend/src/app/components/window/window.constants.ts
@@ -0,0 +1,5 @@
+export class WindowConstants {
+  public static ACTION_CLOSE = "close";
+  public static ACTION_MAXIMISE = "maximise";
+  public static ACTION_MINIMISE = "minimise";
+}
