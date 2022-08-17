310c817e5b351efac54d18727b1423684c771f42
Duwan Sierra <desierrag@gmail.com>
2022-08-16 19:47:07 -0500

    fix: execute data

--- control-projects-backend/src/main/resources/data.sql
+++ control-projects-backend/src/main/resources/data.sql
@@ -1,11 +1,19 @@
---Data Dummy table area interes
-insert into area_interes (area_id, area_nombre) values (1, 'Services');
-insert into area_interes (area_id, area_nombre) values (2, 'Marketing');
-insert into area_interes (area_id, area_nombre) values (3, 'Services');
-insert into area_interes (area_id, area_nombre) values (4, 'Sales');
-insert into area_interes (area_id, area_nombre) values (5, 'Business Development');
-insert into area_interes (area_id, area_nombre) values (6, 'Sales');
-insert into area_interes (area_id, area_nombre) values (7, 'Accounting');
-insert into area_interes (area_id, area_nombre) values (8, 'Services');
-insert into area_interes (area_id, area_nombre) values (9, 'Marketing');
-insert into area_interes (area_id, area_nombre) values (10, 'Research and Development');
+--Create reports
+CREATE
+OR REPLACE VIEW VISTA_DETERMINADO_PROFESOR_PROYECTO AS
+select
+    ROW_NUMBER () OVER (
+           ORDER BY proy.codigo_proyecto
+        ) as id,
+    proy.codigo_grupo as codigogrupo,
+    proy.nombre_grupo as nombregrupo,
+    proy.codigo_proyecto as codigoproyecto,
+    proy.nombre as nombreproyecto,
+    prof.cedula_profesor as cedulaprofesor,
+    prof.nombre as nombreprofesor
+from
+    participa p
+    inner join proyecto_investigacion proy on p.codigo_proyecto = proy.codigo_proyecto
+    inner join proyecto_investigacion proy2 on p.nombre_grupo = proy2.nombre_grupo
+    inner join proyecto_investigacion proy3 on p.codigo_grupo = proy3.codigo_grupo
+    inner join profesor prof on p.cedula_profesor = prof.cedula_profesor
