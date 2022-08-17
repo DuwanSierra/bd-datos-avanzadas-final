17b0105ecbc8f0198bedc0619d28621c3f76516a
Duwan Sierra <desierrag@gmail.com>
2022-08-16 19:45:17 -0500

    merged and first report

--- control-projects-frontend/src/app/views/report-view/report-list/report-list.component.ts
+++ control-projects-frontend/src/app/views/report-view/report-list/report-list.component.ts
@@ -16,16 +16,7 @@ export class ReportListComponent implements OnInit {
 
   profesorDto?: ProfessorRequest;
 
-  data = {
-    labels: ['A', 'B', 'C'],
-    datasets: [
-      {
-        data: [300, 50, 100],
-        backgroundColor: ['#42A5F5', '#66BB6A', '#FFA726'],
-        hoverBackgroundColor: ['#64B5F6', '#81C784', '#FFB74D'],
-      },
-    ],
-  };
+  data?: any;
 
   constructor(
     public profesorManager: ProfesorManagerService,
